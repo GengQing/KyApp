c46761bfbb02adfeb28023991f7effdb17e01ca3
GengQing <716435803@qq.com>
2019-01-18 14:25:16 +0800

    init project

--- src/main/java/pers/geng/qing/ky/Controller.java
+++ src/main/java/pers/geng/qing/ky/Controller.java
@@ -1,4 +1,19 @@
 package pers.geng.qing.ky;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Button btn;
+
+
+    @FXML
+    private TextField showText;
+
+    public void sendOK() {
+        showText.setText("hello world");
+    }
 }
