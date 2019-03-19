f6bc2b8b5e00f5f413240c51153b0cade380cd99
GengQing <716435803@qq.com>
2019-03-19 15:53:59 +0800

    math

--- src/main/resources/findlog.sh
+++ src/main/resources/findlog.sh
@@ -8,6 +8,7 @@ app="application"
 for logFile in ${var} ; do
     echo "${logFile}"
 
+
     if [[ ${logFile} =~ ${app} ]]; then
         lastFile=${logFile}
     else
@@ -17,4 +18,15 @@ done
 
 echo "last=${lastFile}"
 
+
+
+
+stringZ=abcABC123ABCabc
+#       |------|
+#       12345678
+
+echo `expr match "$stringZ" 'abc[A-Z]*.2'`   # 8
+echo `expr "$stringZ" : 'abc[A-Z]*.2'`       # 8
+
+echo ${stringZ:0}
 
