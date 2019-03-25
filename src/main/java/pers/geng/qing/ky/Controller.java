c03f9045c344d5006a542c6ef97ecbfb4076febc
Qing Geng <qing.geng@ericsson.com>
2019-03-25 14:53:30 +0800

    math

--- src/main/java/pers/geng/qing/ky/Controller.java
+++ src/main/java/pers/geng/qing/ky/Controller.java
@@ -1,18 +1,15 @@
 package pers.geng.qing.ky;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
     private Label label;
 
-
-
-    public void sendOK() {
+    @FXML
+    public void showKnowledge() {
 
         label.setText("你改变了状态");
 
